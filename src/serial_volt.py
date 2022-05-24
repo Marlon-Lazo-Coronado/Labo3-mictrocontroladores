25f98763b0f01f556fe9738877830e903a5dbe5f
Marlon-Lazo-Coronado <mlazocoronado@hotmail.com>
2022-05-24 10:27:34 -0700

    Añadimos código para archivar los datos en un csv

---
+++ src/serial_volt.py
@@ -0,0 +1,30 @@
+import numpy as np
+import pandas as pd
+import csv
+
+
+
+file = open("data.csv", "w")
+
+count = 0
+
+vec_data = []
+
+while(1):
+
+
+		sleep(10)
+        
+		data_i = imput_from_micro
+		
+		vec_data.append(data_i)
+		
+		
+		if (count == 20):
+
+				file.write(" ,".join(vec_data))
+				
+				count = 0
+
+    
+    
